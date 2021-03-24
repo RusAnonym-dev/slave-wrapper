30cceccda038f9138305b41e415ae0f5f2f0fd7d
RusAnonym <alexandrsemin2033@gmail.com>
2021-03-24 03:25:01 +0300

    final

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import Account from "./lib/account";
 import config from "./DB/config.json";
 
+// Авторизация аккаунта при помощи параметра authorization из хедеров запросов к рабам
 const account = new Account(config.authorization);
 
 account.updateInfo().then(() => {
+	// Вывод в консоль информации о вашем аккаунте
 	console.log(account.info);
 });
+
+// Логика работы бота здесь
